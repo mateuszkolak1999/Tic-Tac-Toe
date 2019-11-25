b37b34b7e53f2e4a33dab5159a89cd703325d042
Mateusz Kolak <160759@stud.prz.edu.pl>
2019-11-25 23:16:09 +0100

    Dodanie w menu jednoosobowym trybów easy i hard

---
+++ MenuSinglePlayerClass.h
@@ -0,0 +1,33 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+#include <iostream>
+#include "DEFINIONS.h"
+#include "GameState.h"
+
+class MenuSinglePlayerClass
+{
+public:
+	//nazwa gry
+	Texture texture_name;
+	Sprite spriteName;
+
+	//do play
+	Texture textureEasy;
+	Sprite playEasy;
+	Texture textureHard;
+	Sprite playHard;
+
+	//wyjscie
+	Texture textureExit;
+	Sprite exitButton;
+
+	//background
+	Texture textureBackground;
+	Sprite background;
+
+	void loadPlayButton();
+	void loadExitButton();
+	void loadBackground();
+	int play(RenderWindow& window, Event& event, Vector2f mouse);
+};
+
